da98b8ec6daa98dda508f8a2897cd3026b7db664
WMight <wwwyf123456789@163.com>
2020-07-30 17:39:21 +0800

    使用react搭建todoList，并完成添加功能

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+//定义一个react组件
+// function App() {
+//   return (
+//     <div>
+//       hello world !
+//     </div>
+//   );
+// }
+//定义一个react组件必须有render()显示内容，一个App类继承react.component
+//export 导出出去才能在别的中用import 导入
+class TodoList extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      list:[],
+      inputValue:''
+    }
+  }
+
+  handleBtnClick(){
+    this.setState({
+      list:[...this.state.list, this.state.inputValue],  //...是展开运算符
+      inputValue:''
+    })
+  }
+  handleBtnChange(e){
+    this.setState({
+      inputValue:e.target.value
+    })
+  }
+  render(){
+    //jsx语法：在{}中可以写js表达式*只能是表达式语句会报错，也可直接用尖括号
+    //jsx语法：只能return一个标签即最外层是一个div
+    //原生事件是onclick小写，事件绑定是大写的C; this指这个组件
+    return (
+      <div>
+        <div>
+          <input value={this.state.inputValue} onChange={this.handleBtnChange.bind(this)}/>
+          <button onClick={this.handleBtnClick.bind(this)}>add</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+            return <li key={index}>{item}</li>
+            })}
+        </ul>
+      </div>
+    );
+  }
+}
+export default TodoList;
---
+++ todolist/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';//引入react库后面才知道大写字母是组件
+import ReactDOM from 'react-dom';//挂载组件到页面
+import TodoList from './TodoList';//App是最外层组件（大写字母开头）
+
+//组件渲染到html中root标签里去
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
